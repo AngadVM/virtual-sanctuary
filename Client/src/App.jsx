ff6845f14f7a484e90e4147f243ded94042943a8
shailesh43 <sathehshailesh43@gmail.com>
2025-02-05 12:24:04 +0530

    initial frontend

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className='text-4xl rounded-2xl p-2 bg-green-200'>Heyy!</div>
+    </>
+  )
+}
+
+export default App
