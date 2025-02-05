f96b97e690e8231eabe58c0f94dbdb22f1646787
MuizZatam <muizzatam110@gmail.com>
2025-02-05 09:11:22 +0000

    Create GenAI (llm) module

---
+++ App/Modules/llm.py
@@ -0,0 +1,28 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+from time import time
+
+
+load_dotenv()
+api_key = os.environ.get('API')
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+
+def narrate(name: str) -> str:
+
+    response = model.generate_content(f"Acting as David Attenborough, narrate the life of {name} without any markdown formatting")
+
+    return response.text
+
+
+if __name__ == "__main__":
+
+    start = time()
+
+    print(narrate("Red Panda"))
+
+    end = time()
+
+    print(end - start)
