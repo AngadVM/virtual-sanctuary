9279ecce2844e50d21125d6fa9e8f8f001fb7923
shailesh43 <sathehshailesh43@gmail.com>
2025-03-17 20:40:20 +0530

    design LoginPage content

--- Client/src/pages/LoginPage.jsx
+++ Client/src/pages/LoginPage.jsx
@@ -1,14 +1,112 @@
-import React from "react";
+import { useState } from "react";
+import './LoginPage.css'
+import { Typography, Input, Button } from "@material-tailwind/react";
+import { EyeSlashIcon, EyeIcon } from "@heroicons/react/24/solid";
 
-function LoginPage() {
+export function LoginPage() {
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePasswordVisiblity = () => setPasswordShown((cur) => !cur);
+
   return (
-    <div className="bg-black min-h-screen">
-      <div className="mx-auto max-w-2xl py-32">
-        <div className="text-center">
-          <h2 className="text-xl font-semibold text-gray-200">Login Page</h2>
-        </div>
+    <section className="grid text-center h-screen items-center p-8">
+      <div>
+        <Typography variant="h3" color="blue-gray" className="mb-2">
+          Sign In
+        </Typography>
+        <Typography className="mb-16 text-gray-600 font-normal text-[18px]">
+          Enter your email and password to sign in
+        </Typography>
+        <form action="#" className="mx-auto max-w-[24rem] text-left">
+          <div className="mb-6">
+            <label htmlFor="email">
+              <Typography
+                variant="small"
+                className="mb-2 block font-medium text-gray-300"
+              >
+                Your Email
+              </Typography>
+            </label>
+            <Input
+              id="email"
+              color="gray"
+              size="lg"
+              type="email"
+              name="email"
+              placeholder="name@mail.com"
+              className="w-full placeholder:opacity-100 custom-border"
+              labelProps={{
+                className: "hidden",
+              }}
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password">
+              <Typography
+                variant="small"
+                className="mb-2 block font-medium text-gray-300"
+              >
+                Password
+              </Typography>
+            </label>
+            <Input
+              size="lg"
+              placeholder="********"
+              labelProps={{
+                className: "hidden",
+              }}
+              className="w-full placeholder:opacity-100 custom-border"
+              type={passwordShown ? "text" : "password"}
+              icon={
+                <i onClick={togglePasswordVisiblity}>
+                  {passwordShown ? (
+                    <EyeIcon className="h-5 w-5" />
+                  ) : (
+                    <EyeSlashIcon className="h-5 w-5" />
+                  )}
+                </i>
+              }
+            />
+          </div>
+          <Button color="white" size="lg" className="mt-6 text-black bg-gray-200" fullWidth>
+            sign in
+          </Button>
+          <div className="!mt-4 flex justify-end">
+            <Typography
+              as="a"
+              href="#"
+              color="blue-gray"
+              variant="small"
+              className="font-medium"
+            >
+              Forgot password
+            </Typography>
+          </div>
+          <Button
+            variant="outlined"
+            size="lg"
+            className="mt-6 flex h-12 items-center justify-center gap-2 text-gray-300"
+            fullWidth
+          >
+            <img
+              src={`https://www.material-tailwind.com/logos/logo-google.png`}
+              alt="google"
+              className="h-6 w-6"
+            />{" "}
+            sign in with google
+          </Button>
+          <Typography
+            variant="small"
+            color="gray"
+            className="!mt-4 text-center font-normal text-gray-600"
+          >
+            Not registered?{" "}
+            <a href="#" className="font-medium text-gray-200 underline">
+              Create account
+            </a>
+          </Typography>
+        </form>
       </div>
-    </div>
+    </section>
   );
 }
 
